1a62585a86c35df3a140ec6f8774ec32274323a3
Christopher Delgado <chrisdelgado221@gmail.com>
2023-06-17 18:53:46 -0700

    .gitignore is now working

--- build/Debug/Cygwin-Windows/Wordle.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/Wordle.o: Wordle.cpp Wordle.h Tree.h
-
-Wordle.h:
-
-Tree.h:
--- build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp Wordle.h Tree.h
-
-Wordle.h:
-
-Tree.h:
